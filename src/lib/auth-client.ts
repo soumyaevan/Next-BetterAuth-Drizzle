6cfccc4ddb17a75b0242084833bb890d1350c4bb
soumya.sen <soumya.sen@dnb.no>
2026-01-18 23:28:58 +0100

    added some env

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,9 +1,11 @@
 import { createAuthClient } from "better-auth/react";
 import { adminClient } from "better-auth/client/plugins";
 export const authClient = createAuthClient({
-  /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
+  baseURL: process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000",
   plugins: [adminClient()],
 });
 
-export const { signIn, signUp, useSession } = createAuthClient();
+export const { signIn, signUp, useSession } = createAuthClient({
+  baseURL: process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000",
+  plugins: [adminClient()],
+});
